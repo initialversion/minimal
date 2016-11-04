d919aca32fe6d089e2b72ddef0796ac9a6f72f16
Raghu Betina <raghu@firstdraft.com>
2016-11-04 16:02:20 +0000

    Generate notes resource with starter:resource

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,48 @@
+class NotesController < ApplicationController
+  def index
+    @notes = Note.all
+  end
+
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new
+    @note.body = params[:body]
+
+    if @note.save
+      redirect_to "/notes", :notice => "Note created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    @note.body = params[:body]
+
+    if @note.save
+      redirect_to "/notes", :notice => "Note updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+
+    @note.destroy
+
+    redirect_to "/notes", :notice => "Note deleted."
+  end
+end
